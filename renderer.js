30092ae0580abf639ae8f5ecc95e1d4481f82de8
Sébastien <popod@me.com>
2019-03-07 16:26:03 +0100

    printing bug demo

--- renderer.js
+++ renderer.js
@@ -1,3 +1,28 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+let electron = require('electron')
+
+let winOptions = {
+  width: 800,
+  height: 600,
+  show: false
+}
+
+let win = new electron.remote.BrowserWindow(winOptions)
+win.loadURL('file://' + electron.remote.app.getAppPath() + '/print.html')
+
+win.on('closed', () => {
+  win = null
+})
+
+win.webContents.on('did-finish-load', function () {
+  win.webContents.print({}, function (success) {
+    if (!success) {
+      console.log('error')
+    } else {
+      console.log('success')
+    }
+  })
+})
